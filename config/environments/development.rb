0741f53214672ce11e37ca35a8b0c6b2b2f71542
EC2 Default User <ec2-user@ip-172-31-4-152.ec2.internal>
2022-06-20 10:59:52 +0000

    blockhost解消

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
 
   config.hosts << "dc0f37adfd1f4187a868021ca551c271.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "f82ce88e5c6648ada014c3e0940d6f5f.vfs.cloud9.us-east-1.amazonaws.com"
 end
