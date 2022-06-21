7d7dc5ad71dca21b1f153779b88129795d587f0c
EC2 Default User <kakaloto0824@gmail.com>
2022-06-21 05:48:12 +0000

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,14 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
-
+namespace :admin do
+    get "/" => "adimins/homes#top"
+    resources :customers, only: [:show, :index, :edit, :update]
+    resources :products, except: [:destroy]
+    resources :orders, only: [:show, :update]
+    resources :order_products, only: [:update]
+    resources :genres, only: [:create, :index, :edit, :update]
+  end
 
 
 # 顧客用
@@ -18,6 +25,20 @@ devise_for :customers,skip: [:passwords], controllers: {
 
 root to: "publics/homes#top"
 get "/about" => "publics/homes#about"
+
+resources :customers, only: [:show, :edit, :update]
+get "/customers/exit" => "publics/customers#exit"
+
+resources :products, only: [:show, :index]
+resources :shipping_addresses, only: [:index, :edit, :create, :update, :destroy]
+
+resources :cart_products, only: [:index, :create, :update, :destroy]
+delete "/cart_products" => "publics/cart_products#destroy_all"
+
+resources :orders, only: [:index, :show, :new, :create]
+post "/orders/check" => "publics/orders#check"
+get "/orders/complete" => "publics/orders#complete"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
