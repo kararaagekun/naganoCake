cbdac21fc3de420905f67a19df4ef65aa09181f3
EC2 Default User <kakaloto0824@gmail.com>
2022-06-23 05:34:07 +0000

    Add top-view

--- config/routes.rb
+++ config/routes.rb
@@ -22,23 +22,22 @@ devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-
-namespace :publics do
 root to: "publics/homes#top"
 get "/about" => "publics/homes#about"
+namespace :publics do
 
 resource :customers, only: [:show, :edit, :update]
-get "/customers/exit" => "publics/customers#exit"
+get "/customers/exit" => "/customers#exit"
 
 resources :products, only: [:show, :index]
 resources :shipping_addresses, only: [:index, :edit, :create, :update, :destroy]
 
 resources :cart_products, only: [:index, :create, :update, :destroy]
-delete "/cart_products" => "publics/cart_products#destroy_all"
+delete "/cart_products" => "/cart_products#destroy_all"
 
 resources :orders, only: [:index, :show, :new, :create]
-post "/orders/check" => "publics/orders#check"
-get "/orders/complete" => "publics/orders#complete"
+post "/orders/check" => "/orders#check"
+get "/orders/complete" => "/orders#complete"
 end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
