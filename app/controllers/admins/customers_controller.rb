89d0c5be7d65c8bc85e27bcf0824c58a271eff76
EC2 Default User <kakaloto0824@gmail.com>
2022-06-24 04:33:18 +0000

    Add admin-index

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,2 +1,29 @@
 class Admins::CustomersController < ApplicationController
+    
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admins_customer_path(@customer)
+    else
+      edit_admins_customer_path(@customer)
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email, :is_deleted)
+  end
+  
 end
